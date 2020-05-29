1fe7e43f7ae97cca2a48178556254497e958866c
Eloísa Carvalho <eloisactrindade21@gmail.com>
2020-05-29 20:25:48 -0300

    Update nossoarquivode.js

--- nossoarquivode.js
+++ nossoarquivode.js
@@ -1,5 +1,7 @@
 const URL_BASE = 'https://www.wllsistemas.com.br/api/v3/cliente/';
 
+document.onload(carregarTabela())
+
 gravar.addEventListener('click', () => {
 
     if (isValidForm()) {
@@ -15,3 +17,28 @@ gravar.addEventListener('click', () => {
     }
 
 });
+
+function carregarTabela() {
+    fetch(URL_BASE)
+        .then(response => response.json())
+        // .then(json => retornoBotoes.innerHTML = json[0].NOME)
+        .then(json => {
+            json.forEach(cliente => {
+                retornoBotoes.innerHTML += cliente.NOME + '<br>';
+            });
+        })
+}
+
+function editarDado(idDado){
+    fetch(URL_BASE, {
+        method: 'PUT',
+        body: `ID=${idDado}&NOME=${nome.value}&EMAIL=${email.value}&TIPO=${tipo.value}`,
+        headers: {'Content-Type' : 'application/x-www-form-urlencoded'}
+    })
+    .then(response => response.json())
+    .then(json => pResultado.innerHTML = json.mensagem)
+}
+
+function excluirDado(){
+
+}
