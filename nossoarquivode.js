88d5bf6bf87499b35124732b2fe3d48611b24881
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2020-05-29 23:18:17 -0300

    mudança de nome de botao
    Em concordancia com o integrante Anderson, inserimos essa funcionalidade onde o nome do botão muda se a pessoa quiser atualizar

--- nossoarquivode.js
+++ nossoarquivode.js
@@ -38,6 +38,7 @@ gravar.addEventListener('click', () => {
                     statusLimpo()
                     limpaForm()
                     carregarTabela()
+                    gravar.innerHTML = 'Gravar'
                 })
         }
     }
@@ -100,6 +101,7 @@ function inserirDadosForm(idDado) {
             tipo.value = json[0].TIPO
             acao.value = 'atualizar'
             statusLimpo()
+            gravar.innerHTML = 'Atualizar'
         })
 }
 
